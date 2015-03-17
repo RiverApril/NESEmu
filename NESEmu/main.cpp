e486bf5827e6ce8815ef4c7df8e30d03dca09570
Nedearb <nedearb@gmail.com>
2015-03-16 20:40:09 -0700

    Initial Commit

---
+++ NESEmu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NESEmu
+//
+//  Created by Braeden Atlee on 3/16/15.
+//  Copyright (c) 2015 Braeden Atlee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
